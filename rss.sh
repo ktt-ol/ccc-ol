1cb8a1269c1b535a769ccdb356b08882bf2172f9
pge <pge@ring0.de>
2014-01-09 02:13:17 +0100

    improve rss support

--- rss.sh
+++ rss.sh
@@ -2,57 +2,63 @@
 
 ### Searching for txt or html files in blog/ and put them in html und atom-xlm feed ###
 
-blogfile="blog.html"
-rssfile="blog.xml"
+blogfile="web/blog.html"
+rssfile="web/blog.xml"
 
 ## Generate html-blog
 
 # Header
-cat blogHeader.html > $blogfile
+cat web/blogHeader.html > $blogfile
 
 # Content
 for file in blog/*.*; do
-	title=$(basename $file .${file##*.})
-	updated=$(date --date=@`stat -c "%Y" "$file"` "+%Y-%m-%d %H:%M");
-	echo '<div id="'$updated'" class="hero-unit"><h2 style=margin-top:-4%">'$title'</h2><h6>'$updated'</h6>'>> $blogfile;
-	# Markdown txt-file to html
-	if [ ${file##*.} = "txt" ]
+	if [ ${file##*.} = "txt" ] || [ ${file##*.} = "html" ];
 	then
-		markdown $file >> $blogfile;
-	else
-		cat $file >> $blogfile
+		title=$(basename $file .${file##*.})
+		updated=$(date --date=@`stat -c "%Y" "$file"` "+%Y-%m-%d %H:%M");
+		echo '<div id="'$updated'" class="hero-unit"><h2 style=margin-top:-4%">'$title'</h2><h6>'$updated'</h6>'>> $blogfile;
+		# Markdown txt-file to html
+		if [ ${file##*.} = "txt" ];
+		then
+			markdown $file >> $blogfile;
+		else
+			cat $file >> $blogfile
+		fi
+		echo '</div>' >> $blogfile;
 	fi
-	echo '</div>' >> $blogfile;
 done
 
 # Footer
-cat blogFooter.html >> $blogfile;
+cat web/blogFooter.html >> $blogfile;
 
 ## Generate xml-atom-feed (last 10 txt-files)
 
 # Header
-cat blogHeader.xml > $rssfile;
+cat web/blogHeader.xml > $rssfile;
 date=`date +%FT%T%:z`;
 echo '<updated>'$date'</updated>' >> $rssfile;
 
-# Content #TODO last 10
+# Content #TODO write only last 10 in rss feed
 for file in blog/*.*; do
-	title=$(basename $file .${file##*.})
-	updated=$(date --date=@`stat -c "%Y" "$file"` "+%FT%T%:z");
-	echo '<entry>
-			<title>'$title'</title>
-			<link href="https://ccc-ol.de/blog.html#'$updated'"/>
-			<id>https://ccc-ol.de/blog.html#'$updated'</id>
-			<updated>'$updated'</updated>
-			<content type="html"><![CDATA[' >> $rssfile;
-	# Markdown txt-file to html and replace CDATA endtag
-	if [ ${file##*.} = "txt" ]
+	if [ ${file##*.} = "txt" ] || [ ${file##*.} = "html" ];
 	then
-		markdown $file | tr ']]>' '))>' >> $rssfile;
-	else
-		cat $file | tr ']]>' '))>' >> $rssfile
+		title=$(basename $file .${file##*.})
+		updated=$(date --date=@`stat -c "%Y" "$file"` "+%FT%T%:z");
+		echo '<entry>
+				<title>'$title'</title>
+				<link href="https://ccc-ol.de/blog.html#'$updated'"/>
+				<id>https://ccc-ol.de/blog.html#'$updated'</id>
+				<updated>'$updated'</updated>
+				<content type="html"><![CDATA[' >> $rssfile;
+		# Markdown txt-file to html and replace CDATA endtag
+		if [ ${file##*.} = "txt" ];
+		then
+			markdown $file | tr ']]>' '))>' >> $rssfile;
+		else
+			cat $file | tr ']]>' '))>' >> $rssfile
+		fi
+		echo ']]></content></entry>' >> $rssfile;
 	fi
-	echo ']]></content></entry>' >> $rssfile;
 done
 
 # Footer
